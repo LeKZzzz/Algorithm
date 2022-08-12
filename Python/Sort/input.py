494acdb6c81618a6506c0243d51ea7512fcdf747
LeK <1148579949@qq.com>
2022-08-12 21:26:02 +0800

    2022/8/12更新

--- Python/Sort/input.py
+++ Python/Sort/input.py
@@ -3,9 +3,10 @@
 # 创建日期：2022/8/11
 
 import random
-import quick_sort_right
+import quick_sort_random
 
 list = [i for i in range(21)]
 random.shuffle(list)
-quick_sort_right.sort(list,0,len(list)-1)
+list.append(1)
+quick_sort_random.sort(list, 0, len(list) - 1)
 print(list)
--- Python/Sort/quick_sort_random.py
+++ Python/Sort/quick_sort_random.py
@@ -10,28 +10,17 @@ def sort(list, left, right):
         return
 
     mid = random.randint(left, right)
-    while mid == left:
-        mid = random.randint(left, right)
     tmp = list[mid]
-    t = 0
     leftpoint = left
     rightpoint = right
-    while leftpoint != rightpoint:
-        while list[leftpoint] <= tmp and leftpoint < mid:
+    while leftpoint < rightpoint:
+        while list[leftpoint] < tmp and leftpoint < rightpoint:
             leftpoint += 1
-        if leftpoint != rightpoint:
-            if t == 0:
-                list[mid] = list[leftpoint]
-                t = 1
-            else:
-                list[rightpoint] = list[leftpoint]
-                rightpoint -= 1
-        while list[rightpoint] >= tmp and leftpoint < rightpoint:
+        while list[rightpoint] > tmp and leftpoint < rightpoint:
             rightpoint -= 1
-        if leftpoint != rightpoint:
-                list[leftpoint] = list[rightpoint]
-                leftpoint += 1
-    list[leftpoint] = tmp
+        list[leftpoint], list[rightpoint] = list[rightpoint], list[leftpoint]
+        if list[leftpoint] == list[rightpoint] == tmp:
+            leftpoint += 1
     print(list)
     sort(list, rightpoint + 1, right)
     sort(list, left, leftpoint - 1)
